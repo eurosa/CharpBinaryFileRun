ad5c3ff0088234dfbaeb2d4f5210d13d1164e671
User <User@WINDOWS-3K3TV45>
2023-03-14 17:06:29 +0530

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LaunchCommandLineApp();
+        }
+
+
+        /// <summary>
+        /// Launch the application with some options set.
+        /// </summary>
+        static void LaunchCommandLineApp()
+        {
+            // For the example
+            const string ex1 = "C:\\";
+            const string ex2 = "C:\\Dir";
+
+            // Use ProcessStartInfo class
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.FileName = "yt-dlp.exe";
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startInfo.Arguments = "https://youtu.be/-TqQk5CfQBI";
+
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using statement will close.
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                }
+            }
+            catch
+            {
+                // Log error.
+            }
+        }
+    }
+}
